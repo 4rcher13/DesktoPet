d0e29ac491c5d9b6cc3015130f9267dbd04fdcdd
Chucho <iquarus2@gmail.com>
2026-09-10 18:32:00 -0600

    Intentado programar en C# ocupando POO intentando crear una mascotita

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+namespace DesktoPet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    private void Window_MouseLeftButtonDown (object sender,MouseButtonEventArgs e) //Metodo para con el mause mover la figura
+        {
+            DragMove();
+        }
+    }
+}
